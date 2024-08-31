65e66d2e1e85bc9090440cf73ce0e03e833538d6
FedorMAshoshin <fmashoshin@gmail.com>
2024-08-30 22:12:00 -0700

    Fix password issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password_digest, presence: true
 end
