51ee675966d030b08690da598ea50804ba845251
FedorMAshoshin <fmashoshin@gmail.com>
2024-08-30 22:35:35 -0700

    recreate products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_29_061717) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_31_053109) do
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "email"
